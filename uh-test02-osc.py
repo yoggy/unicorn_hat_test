99213fddc92f6e11573935182a0e6ee83d0dc7f9
yoggy <yoggy0@gmail.com>
2015-02-03 10:25:09 +0000

    modified:   uh-test02-osc.py

--- uh-test02-osc.py
+++ uh-test02-osc.py
@@ -3,54 +3,98 @@
 # uh-test02-osc.py - simple OSC server for Unicorn HAT
 #
 # Unicorn HAT
-#   http://shop.pimoroni.com/products/unicorn-hat
-#   https://github.com/pimoroni/UnicornHat
+#     http://shop.pimoroni.com/products/unicorn-hat
+#     https://github.com/pimoroni/UnicornHat
 #
 # setup
-#   $ sudo -i
-#   # \curl -sS get.pimoroni.com/unicornhat | bash
-#   # exit
-#   $ sudo pip-3.2 install python-osc
+#     $ sudo -i
+#     # \curl -sS get.pimoroni.com/unicornhat | bash
+#     # exit
+#     $ sudo pip-3.2 install python-osc
 #
 import unicornhat as uh
 from time      import sleep
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-def set_brightness(addr, b):
-  print("set_brightness() : b={0}".format(b))
+def set_brightness(b):
   uh.brightness(b)
   uh.show()
 
-def set_clear(addr):
-  print("set_clear()")
-  set_color(None, 0, 0, 0)
-
-def set_color(addr, r, g, b):
-  print("set_color() : r={0}, g={1}, b={2}".format(r, g, b))
+def set_color(r, g, b):
   for y in range(8):
     for x in range(8):
       uh.set_pixel(x, y, r, g, b)
   uh.show()
 
-def set_flash(addr, t, r, g, b):
-  print("set_flash() : t={0}, r={1}, g={2}, b={3}".format(t, r, g, b))
-  set_color(None, 255, 255, 255)
+#
+# /brightness handler
+#
+# args
+#     float brightness (0.0-1.0)
+# ex.
+#     $ sendosc 192.168.1.100 7000 /brightness f 0.3
+#
+def handle_brightness(addr, b):
+  print("/brightness : addr={0}, b={1}".format(addr, b))
+  set_brightness(b)
+
+#
+# /clear handler
+#
+# args
+#     None
+# ex.
+#     $ sendosc 192.168.1.100 7000 /clear 
+#
+def handle_clear(addr):
+  print("/clear : addr={0}".format(addr))
+  set_color(0, 0, 0)
+
+#
+# /color handler
+# 
+# args
+#     int   red   (0-255)
+#     int   green (0-255)
+#     int   blue  (0-255)
+#
+# ex.
+#     $ sendosc 192.168.1.100 7000 /color i 255 i 255 i 255
+#
+def handle_color(addr, r, g, b):
+  print("/color : addr={0}, r={1}, g={2}, b={3}".format(addr, r, g, b))
+  set_color(r, g, b)
+
+#
+# /flash handler
+# 
+# args
+#     float time  (sec)
+#     int   red   (0-255)
+#     int   green (0-255)
+#     int   blue  (0-255)
+#
+# ex.
+#     $ sendosc 192.168.1.100 7000 /flash f 0.03 i 255 i 255 i 255
+#
+def handle_flash(addr, t, r, g, b):
+  print("/flash : addr={0}, t={1}, r={2}, g={3}, b={4}".format(addr, t, r, g, b))
+  set_color(r, g, b)
   sleep(t)
-  set_clear(None)
+  set_color(0, 0, 0)
 
 if __name__ == "__main__":
   uh.brightness(.2)
-  set_clear(None)
+  set_color(0, 0, 0)
 
   dispatcher = dispatcher.Dispatcher()
 
   dispatcher.map("/debug", print)
-  dispatcher.map("/brightness", set_brightness)
-  dispatcher.map("/clear",      set_clear)
-  dispatcher.map("/color",      set_color)
-  dispatcher.map("/flash",      set_flash)
-  #dispatcher.map("/pixel",      set_pixel)
+  dispatcher.map("/brightness", handle_brightness)
+  dispatcher.map("/clear",      handle_clear)
+  dispatcher.map("/color",      handle_color)
+  dispatcher.map("/flash",      handle_flash)
 
   server = osc_server.ThreadingOSCUDPServer(
       ("0.0.0.0", 7000), dispatcher)
